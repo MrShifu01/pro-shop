eb9ed54de1ed0a7adbf475d810a2cee813073b80
ChristianStander <stander.christian@gmail.com>
2023-05-04 07:11:33 +0200

    redux starting

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from "express" // add type:module to be able to do this
-import products from "./data/products.js"
 import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from "./config/db.js"
+import productRoutes from "./routes/productRoutes.js"
+import { notFound, errorHandler } from "./middleware/errorHandler.js"
 
 const port = process.env.PORT || 5000
 
-connectDB() // COnnect to Mongo DB
+connectDB() // Connect to Mongo DB
 
 const app = express()
 
@@ -14,14 +15,11 @@ app.get("/", (req, res) => {
     res.send("API is running")
 })
 
-app.get("/api/products", (req, res) => {
-    res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get("/api/products/:id", (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
+app.use(notFound)
+app.use(errorHandler)
+
 
 app.listen(port, () => {
     console.log(`Server running in development mode on port ${port}`);
